bcca160f3843bc293838c98851939cc02627e761
mlatinov <latinovmetodi@gmail.com>
2026-04-20 23:51:51 +0300

    update the model experiment adding the model with two stages of training

--- scripts/experiments/exp_baseline.py
+++ scripts/experiments/exp_baseline.py
@@ -1,7 +1,9 @@
+
 import mlflow
 import tensorflow as tf
 from scripts.helpers.process_image import process_image
 from scripts.helpers.plot_image_aug import plot_augmented_samples
+from scripts.cnns_models.EfficientNetV2B0 import transfer_efficient_net
 
 def exp_baseline() :
 
@@ -15,6 +17,8 @@ def exp_baseline() :
         model_data = process_image(
             training_dir = "data/Training/",
             testing_dir  = "data/Testing/",
+            rescale      =  None, # Efficient Net handles it internally 
+            color_mode   = "rgb", # To match the model chanels expectations 
             target_image_size = (254, 254)
         )
 
@@ -22,4 +26,68 @@ def exp_baseline() :
         image_augmented = plot_augmented_samples(
             data_generator = model_data["training_set"],
             n_samples      = 4
+        )
+
+        # Get the model 
+        effecient_net = transfer_efficient_net(input_shape = (254, 254, 3))
+
+       # Compile the model for Stage one Training the Head 
+        effecient_net.compile(
+            optimizer =  tf.keras.optimizers.Adam(learning_rate = 0.001),
+            loss      = tf.keras.losses.CategoricalCrossentropy(),
+            metrics   = [
+                tf.keras.metrics.AUC(),
+                tf.keras.metrics.Recall(),
+                tf.keras.metrics.Precision()
+            ]
+        )
+        ## Stage 1 Train the head 
+        history_stage_1 = effecient_net.fit(
+            x                = model_data["training_set"],
+            validation_data  = model_data["validation_set"],
+            epochs           = 10,
+            verbose          = 1,
+            callbacks = [
+                tf.keras.callbacks.EarlyStopping(
+                    monitor              = "val_loss",
+                    patience             = 3,
+                    restore_best_weights = True,
+                    verbose              = 1
+                )
+            ]
+        )
+        ## Stage 2 Fine Tune 
+
+        # Unfreeze the last 20 layers 
+        base = effecient_net.get_layer("efficientnetv2-b0")
+
+        effecient_net.trainable = True
+
+        for layer in effecient_net.layers[:-20] :
+            layer.trainable = False
+
+        # Recompile the model with Lower LR
+        effecient_net.compile(
+            optimizer = tf.keras.optimizers.Adam(learning_rate = 1e-5),
+            loss      = tf.keras.losses.CategoricalCrossentropy(),
+            metrics   = [tf.keras.metrics.AUC()]
+        )
+
+        # Retrain the model with unfreezed layers
+        history_stage2 = effecient_net.fit(
+             x               = model_data["training_set"],
+             validation_data = model_data["validation_set"],
+             epochs          = 10,
+             callbacks       = [
+                tf.keras.callbacks.EarlyStopping(
+                    monitor              = "val_loss",
+                    patience             = 3,
+                    restore_best_weights = True
+                ),
+                tf.keras.callbacks.ReduceLROnPlateau(
+                    monitor  = "val_loss",
+                    factor   = 0.5,
+                    patience = 2
+                )
+            ]
         )
